7ddc7a4637401d0ab8e46c63a2a63f914163ef1a
gzamudioc <gzamudioc@nikkenlatam.com>
2024-06-10 16:51:12 -0600

    ajuste rutas

--- app/Http/Controllers/generalController.php
+++ app/Http/Controllers/generalController.php
@@ -17,50 +17,13 @@ class generalController extends Controller
         //
         //dd("ss");
         
-        return view('reportes.seguimiento-organizacion');
+        return view('reportes.seguimientoOrganizacion');
     }
 
 
     public function get_seguimiento_organizacion(Request $request){
 
-        $results = DB::connection('75')
-        ->table('dwt_estrategiareto4x4')
-            ->select(
-                'associateid',
-                'associateName',
-                'tipo',
-                DB::raw("CASE WHEN rangoSocio = 9 THEN 'DRL'
-                              WHEN rangoSocio = 8 THEN 'DIA'
-                              WHEN rangoSocio = 7 THEN 'PLO'
-                              WHEN rangoSocio = 6 THEN 'ORO'
-                              WHEN rangoSocio = 5 THEN 'PLA'
-                              WHEN rangoSocio = 3 THEN 'EXE'
-                              WHEN rangoSocio = 2 THEN 'SUP'
-                              ELSE 'DIR' END AS rangoSocio"),
-                'pais',
-                'telefono',
-                'email',
-                'sponsorid',
-                'sponsorName',
-                DB::raw("CASE WHEN rangoSponsor = 9 THEN 'DRL'
-                              WHEN rangoSponsor = 8 THEN 'DIA'
-                              WHEN rangoSponsor = 7 THEN 'PLO'
-                              WHEN rangoSponsor = 6 THEN 'ORO'
-                              WHEN rangoSponsor = 5 THEN 'PLA'
-                              WHEN rangoSponsor = 3 THEN 'EXE'
-                              WHEN rangoSponsor = 2 THEN 'SUP'
-                              ELSE 'DIR' END AS rangoSponsor"),
-                DB::raw("CASE WHEN semana_1 = 1 THEN 'SI' ELSE 'NO' END AS semana_1"),
-                DB::raw("CASE WHEN semana_2 = 1 THEN 'SI' ELSE 'NO' END AS semana_2"),
-                DB::raw("CASE WHEN semana_3 = 1 THEN 'SI' ELSE 'NO' END AS semana_3"),
-                DB::raw("CASE WHEN semana_4 = 1 THEN 'SI' ELSE 'NO' END AS semana_4"),
-                DB::raw("CASE WHEN semana_5 = 1 THEN 'SI' ELSE 'NO' END AS semana_5"),
-                DB::raw("CASE WHEN ganador = 1 THEN 'SI' ELSE 'NO' END AS ganador")
-            )
-            ->get();
-
-        //dd($results);
-        return response()->json($results);
+        
         
 
     }
--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,8 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/seguimiento-organizacion', [generalController::class, 'index_seguimiento_organizacion'])->name('seguimiento-organizacion.index');
-Route::get('/get-seguimiento-organizacion', [generalController::class, 'get_seguimiento_organizacion'])->name('seguimiento-organizacion.get');
+Route::get('/seguimientoOrganizacion', [generalController::class, 'index_seguimiento_organizacion'])->name('seguimiento-organizacion.index');
+Route::get('/getSeguimientoOrganizacion', [generalController::class, 'get_seguimiento_organizacion'])->name('seguimiento-organizacion.get');
 
-Route::get('/seguimiento-staff', [generalController::class, 'index_seguimiento_staff'])->name('seguimiento-staff.index');
-Route::get('/seguimiento-personal', [generalController::class, 'index_seguimiento_personal'])->name('seguimiento-personal.index');
+Route::get('/seguimientoStaff', [generalController::class, 'index_seguimiento_staff'])->name('seguimiento-staff.index');
+Route::get('/seguimientoPersonal', [generalController::class, 'index_seguimiento_personal'])->name('seguimiento-personal.index');
