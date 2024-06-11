eeb6ecb13b9889e28a24dd29c4a7634406b9f16f
gzamudioc <gzamudioc@nikkenlatam.com>
2024-06-11 17:47:10 -0600

    export

--- app/Exports/SeguimientoStaffExport.php
+++ app/Exports/SeguimientoStaffExport.php
@@ -17,42 +17,43 @@ class SeguimientoStaffExport implements FromQuery, WithHeadings, WithMapping
 
     public function query()
     {
-        $query = "
+        $subQuery = "
             SELECT 
-            associateid,
-            associateName,
-            tipo,
-            CASE WHEN rangoSocio = 9 THEN 'DRL'
-                 WHEN rangoSocio = 8 THEN 'DIA'
-                 WHEN rangoSocio = 7 THEN 'PLO'
-                 WHEN rangoSocio = 6 THEN 'ORO'
-                 WHEN rangoSocio = 5 THEN 'PLA'
-                 WHEN rangoSocio = 3 THEN 'EXE'
-                 WHEN rangoSocio = 2 THEN 'SUP'
-            ELSE 'DIR' END AS rangoSocio,
-            pais,
-            telefono,
-            email,
-            sponsorid, 
-            sponsorName,
-            CASE WHEN rangoSponsor = 9 THEN 'DRL'
-                 WHEN rangoSponsor = 8 THEN 'DIA'
-                 WHEN rangoSponsor = 7 THEN 'PLO'
-                 WHEN rangoSponsor = 6 THEN 'ORO'
-                 WHEN rangoSponsor = 5 THEN 'PLA'
-                 WHEN rangoSponsor = 3 THEN 'EXE'
-                 WHEN rangoSponsor = 2 THEN 'SUP'
-            ELSE 'DIR' END AS rangoSponsor,
-            CASE WHEN semana_1= 1 THEN 'SI' ELSE 'NO' END AS semana_1,
-            CASE WHEN semana_2= 1 THEN 'SI' ELSE 'NO' END AS semana_2,
-            CASE WHEN semana_3= 1 THEN 'SI' ELSE 'NO' END AS semana_3,
-            CASE WHEN semana_4= 1 THEN 'SI' ELSE 'NO' END AS semana_4,
-            CASE WHEN semana_5= 1 THEN 'SI' ELSE 'NO' END AS semana_5,
-            CASE WHEN ganador= 1 THEN 'SI' ELSE 'NO' END AS ganador
+                associateid,
+                associateName,
+                tipo,
+                CASE WHEN rangoSocio = 9 THEN 'DRL'
+                     WHEN rangoSocio = 8 THEN 'DIA'
+                     WHEN rangoSocio = 7 THEN 'PLO'
+                     WHEN rangoSocio = 6 THEN 'ORO'
+                     WHEN rangoSocio = 5 THEN 'PLA'
+                     WHEN rangoSocio = 3 THEN 'EXE'
+                     WHEN rangoSocio = 2 THEN 'SUP'
+                ELSE 'DIR' END AS rangoSocio,
+                pais,
+                telefono,
+                email,
+                sponsorid, 
+                sponsorName,
+                CASE WHEN rangoSponsor = 9 THEN 'DRL'
+                     WHEN rangoSponsor = 8 THEN 'DIA'
+                     WHEN rangoSponsor = 7 THEN 'PLO'
+                     WHEN rangoSponsor = 6 THEN 'ORO'
+                     WHEN rangoSponsor = 5 THEN 'PLA'
+                     WHEN rangoSponsor = 3 THEN 'EXE'
+                     WHEN rangoSponsor = 2 THEN 'SUP'
+                ELSE 'DIR' END AS rangoSponsor,
+                CASE WHEN semana_1= 1 THEN 'SI' ELSE 'NO' END AS semana_1,
+                CASE WHEN semana_2= 1 THEN 'SI' ELSE 'NO' END AS semana_2,
+                CASE WHEN semana_3= 1 THEN 'SI' ELSE 'NO' END AS semana_3,
+                CASE WHEN semana_4= 1 THEN 'SI' ELSE 'NO' END AS semana_4,
+                CASE WHEN semana_5= 1 THEN 'SI' ELSE 'NO' END AS semana_5,
+                CASE WHEN ganador= 1 THEN 'SI' ELSE 'NO' END AS ganador
             FROM dwt_estrategiareto4x4
-            ORDER BY associateid"; // Asegúrate de que estás ordenando por un campo relevante
-          
-        return DB::connection('75')->table(DB::raw("($query) as sub"));
+            ORDER BY associateid";
+
+        return DB::connection('75')->table(DB::raw("($subQuery) as sub"))
+            ->orderBy('associateid');
     }
 
     public function headings(): array
